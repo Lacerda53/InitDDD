9130974879bde4c4e607beffa1dcfc8fdc9a46e2
Lacerda53 <guilhermeandrade2013@gmail.com>
2020-07-01 19:53:16 -0300

    first commit

---
+++ PaymentContext.Domain/Entities/PaypalPayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class PaypalPayment : Payment
+    {
+        public PaypalPayment(string transactionCode, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string document, string payer, string address, string email) : base(paidDate, expireDate, total, totalPaid, document, payer, address, email)
+        {
+            TransactionCode = transactionCode;
+        }
+
+        public string TransactionCode { get; private set; }
+    }
+}
